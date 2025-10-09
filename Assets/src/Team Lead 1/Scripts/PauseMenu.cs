4bf16d336c2fb4a1ad3af0b4ec73b52433c17167
aydenkelley <aydenfkelley@gmail.com>
2025-10-08 21:54:29 -0700

    Basic pause menu

---
+++ Assets/src/Team Lead 1/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject MenuScreen;
+    [SerializeField] private Button Resume;
+    [SerializeField] private Button Quit;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        MenuScreen.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            if(MenuScreen.activeInHierarchy == false)
+            {
+                MenuScreen.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+    }
+
+    public void Unpause()
+    {
+        MenuScreen.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
