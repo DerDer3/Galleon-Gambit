01294b826fda9914d1b2b48e4391ad62a52e41fe
DerDer3 <djmcnee3@gmail.com>
2025-10-26 15:34:30 -0700

    idk anymore

---
+++ Assets/src/Team Lead 2/Tests/Scripts/CardObject.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class CardObject : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    private Card linkedCard; // the card this object represents
+    private GameState linkedState;
+    private SpriteRenderer spriteRenderer;
+    public TextMeshProUGUI cardName;
+    public TextMeshProUGUI manaAmount;
+    Vector3 currentPos;
+
+    public void SetCard(Card card, GameState state)
+    {
+        linkedCard = card;
+        linkedState = state;
+        cardName.text = linkedCard.cardName;
+        manaAmount.text = "" + linkedCard.cardCost;
+
+        float randomGrayValue = Random.Range(0.2f, 0.8f);
+        this.ChangeColor(randomGrayValue, randomGrayValue, randomGrayValue);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+      if(linkedState.turn == false)
+      {
+        linkedCard.Play(linkedState);
+        linkedState.mainDeck.DiscardCard(linkedCard);
+        SetCard(linkedState.mainDeck.DrawCardWithReshuffle(), linkedState);
+      }
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+      transform.position += Vector3.up;
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+      transform.position = currentPos;
+    }
+
+    public void ChangeColor(float r, float g, float b)
+    {
+      spriteRenderer = GetComponent<SpriteRenderer>();
+      spriteRenderer.color = new Color(r, g, b, 1); 
+    }
+
+    void Start()
+    {
+      currentPos = transform.position;
+    }
+
+    void Update()
+    {
+    }
+}
