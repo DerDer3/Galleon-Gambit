5ab522c9cdb35682bcd1f404424c580da6f76d30
Anne <231733724+Foxsadilla@users.noreply.github.com>
2025-10-30 10:44:41 -0700

    kyuydf

--- Assets/src/Team Lead 2/Scripts/GameManager.cs
+++ Assets/src/Team Lead 2/Scripts/GameManager.cs
@@ -5,10 +5,31 @@ public class GameManager : MonoBehaviour
 {
     public GameObject cardPrefab;
     public GameState mainGame;
-    List<GameObject> cardHand = new List<GameObject>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+      mainGame.mainDeck.AddCard(new Slash(), "Attack");
+      mainGame.mainDeck.AddCard(new Slash(), "Attack");
+      mainGame.mainDeck.AddCard(new Slash(), "Attack");
+      mainGame.mainDeck.AddCard(new Slash(), "Attack");
+      mainGame.mainDeck.AddCard(new Slash(), "Attack");
+      mainGame.mainDeck.AddCard(new ShipRepair(), "Repair");
+      mainGame.mainDeck.AddCard(new ShipRepair(), "Repair");
+      mainGame.mainDeck.AddCard(new ShipRepair(), "Repair");
+      mainGame.mainDeck.AddCard(new ShipRepair(), "Repair");
+      mainGame.mainDeck.AddCard(new ShipRepair(), "Repair");
+
+      mainGame.mainDeck.Shuffle(mainGame.mainDeck);
+
+      int index = 0;
+      for(int i = 0; i < 5; i++)
+      {
+        GameObject newCard = Instantiate(cardPrefab);
+        CardObject cardObject = newCard.GetComponent<CardObject>();
+        cardObject.SetCard(mainGame.mainDeck.DrawCardWithReshuffle(), mainGame);
+        newCard.transform.localPosition = new Vector3((index * 2) -4, -3, 0);
+        index += 1;
+      }
     }
 
     // Update is called once per frame
@@ -25,40 +46,5 @@ public class GameManager : MonoBehaviour
        {
          mainGame.turn = false;
        }
-    }
-
-    void DrawHandCard()
-    {
-        GameObject newCard = Instantiate(cardPrefab);
-        CardObject cardObject = newCard.GetComponent<CardObject>();
-        cardObject.SetCard(mainGame.mainDeck.DrawCardWithReshuffle(), mainGame);
-        cardHand.Add(newCard);
-
-        UpdateHandLayout();
-    }
-
-    void UpdateHandLayout()
-    {
-      int n = cardHand.Count;
-
-      if(n == 0) return;
-
-      // float maxFanAngle = 25f;
-      float totalWidth = 14f;
-      float spacing = totalWidth / Mathf.Max(n - 1, 1);
-      float yOffset = -3f;
-
-      // float angleStep = n > 1 ? maxFanAngle / (n - 1) : 0f;
-      // float startAngle = -maxFanAngle / 2f;
-      float startX = -(n - 1) * spacing / 2f;
-
-      for(int i = 0; i < n; i++)
-      {
-        // float angle = startAngle + i * angleStep;
-        float xPos = startX + i * spacing;
-
-        cardHand[i].transform.localPosition = new Vector3(xPos, yOffset, 0);
-        // cardHand[i].transform.localRotation = Quaternion.Euler(0, 0, -angle);
-      }
     }
 }
