e1e539bcb7e18330bdd22574b59ce03d8b4f8310
Anne <231733724+Foxsadilla@users.noreply.github.com>
2025-11-12 10:29:29 -0800

    Revert "fdgdf"
    This reverts commit 993843915d5ee2c74a54a4f4b9e5652f51f598d0.

---
+++ Assets/src/Team Lead 2/Scripts/Card.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public abstract class Card
+{
+  // Public here ensures they can all be read, but set is private
+  public string cardName { get; private set;}
+  public string cardRarity { get; private set;}
+  public int cardCost { get; private set;}
+  public string cardDescription { get; private set;}
+  public int cardLevel { get; private set;}
+
+  protected Card(string cardName, string cardRarity, int cardCost, string cardDescription, int cardLevel)
+  {
+    this.cardName = cardName;
+    this.cardRarity = cardRarity;
+    this.cardCost = cardCost;
+    this.cardDescription = cardDescription;
+    this.cardLevel = cardLevel;
+  }
+
+  public abstract void Play(GameState state);
+  public void Upgrade()
+  {
+    this.cardLevel += 1;
+  }
+}
---
+++ Assets/src/Team Lead 2/Scripts/CardObject.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class CardObject : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    private Card linkedCard; // the card this object represents
+    private GameState linkedState;
+    private SpriteRenderer spriteRenderer;
+    public TextMeshProUGUI cardName;
+    public TextMeshProUGUI manaAmount;
+    Vector3 currentPos;
+
+    //public GameObject pauseObject;
+
+    public void SetCard(Card card, GameState state)
+    {
+        linkedCard = card;
+        linkedState = state;
+        cardName.text = linkedCard.cardName;
+        manaAmount.text = "" + linkedCard.cardCost;
+
+        float randomGrayValue = Random.Range(0.2f, 0.8f);
+        this.ChangeColor(randomGrayValue, randomGrayValue, randomGrayValue);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (linkedState.turn == false)
+        {
+            linkedCard.Play(linkedState);
+            linkedState.mainDeck.DiscardCard(linkedCard);
+            SetCard(linkedState.mainDeck.DrawCardWithReshuffle(), linkedState);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        if (!checkPause())
+        {
+            transform.position += Vector3.up;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        transform.position = currentPos;
+    }
+
+    public void ChangeColor(float r, float g, float b)
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = new Color(r, g, b, 1);
+    }
+
+    void Start()
+    {
+        currentPos = transform.position;
+    }
+
+    public bool checkPause()
+    {  /*
+        if(pauseObject != null)
+        {
+            if (pauseObject.activeSelf)
+            {
+                return true;
+            }
+            else if (pauseObject.activeInHierarchy)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }*/
+        return false;
+        //Eventually will update to work correctly again.
+    }
+}
